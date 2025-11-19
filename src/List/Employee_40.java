868e648c1543dfc65f34243a21e3eae9550f6945
Pratiksha-Prakash-Jadhav <pratikshajadhav0372@gmail.com>
2025-11-19 19:03:15 +0530

    40 Questions Solve

--- src/List/Employee_40.java
+++ src/List/Employee_40.java
@@ -1,5 +1,46 @@
 package List;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Employee_40 {
 
+    static class Employee {
+        String name;
+        String department;
+        Double salary;
+
+        Employee(String name, String department, Double salary) {
+            this.name = name;
+            this.department = department;
+            this.salary = salary;
+        }
+
+        String getName() { return name; }
+        String getDepartment() { return department; }
+        Double getSalary() { return salary; }
+    }
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("Rohit", "HR", 12000.00),
+                new Employee("Sneha", "IT", 78000.00),
+                new Employee("Amit", "Finance", 90000.00),
+                new Employee("Priya", "IT", 12344.00),
+                new Employee("Kiran", "HR", 45637.00)
+        );
+
+        // Calculate total salary per department
+        Map<String, Double> totalSalary =
+                employees.stream()
+                        .collect(Collectors.groupingBy(
+                                Employee::getDepartment,
+                                Collectors.summingDouble(Employee::getSalary)
+                        ));
+
+        System.out.println(totalSalary);
+    }
 }
