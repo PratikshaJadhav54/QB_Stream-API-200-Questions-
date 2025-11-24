41495a3d84d0ab7c10888a2fc031719324d988ee
Pratiksha-Prakash-Jadhav <pratikshajadhav0372@gmail.com>
2025-11-24 23:17:43 +0530

    Basic Questions solve

--- src/Set/AllSetQuestions.java
+++ src/Set/AllSetQuestions.java
@@ -26,12 +26,30 @@ public class AllSetQuestions {
 		uppercase.add("mrudula");
 		uppercase.add("Raju");
 		
-		System.out.println("All Set element print in uppercase letter");
+		System.out.println("\n\nAll Set element print in uppercase letter");
 		uppercase.stream().map(x -> x.toUpperCase()).forEach(x -> System.out.print(x+", "));
 		
-		//3.
+		//3.Filter and print only even numbers from a Set<Integer>
+		Set<Integer> evennumber=new HashSet<Integer>();
+		evennumber.add(12);
+		evennumber.add(34);
+		evennumber.add(90);
+		evennumber.add(77);
+		evennumber.add(24);
 		
+		System.out.println("\n\nPrint even number only");
+		evennumber.stream().filter(x -> x%2==0).forEach(x -> System.out.print(x+", "));
 		
+		//4.Count how many Strings in a Set<String> start with the letter "A"
+		Set<String> startWith=new HashSet<String>();
+		startWith.add("Pooja");
+		startWith.add("Akash");
+		startWith.add("Akshay");
+		startWith.add("Prakash");
+		startWith.add("Ramu");
+		
+		System.out.println("\n\nPrint only name start with A");
+		startWith.stream().filter(x -> x.startsWith("A")).forEach(x -> System.out.print(x+", "));
 		
 		
 		
